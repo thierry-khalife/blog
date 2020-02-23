1ee916c03f816ee3ab7dfd4921d8ce6d274dd6c2
nicolas-camilloni <nicolas.camilloni@laplateforme.io>
2020-02-23 01:22:28 +0100

    avancé css header

--- header.php
+++ header.php
@@ -1,36 +1,48 @@
 <header>
         <nav class="nav">
-            <section class="headerleft">
-                <section id="logo">
-                <a href="index.php">Accueil</a>
+            <section class="navbtn">
+                <a class="link1" href="index.php">Accueil</a>
+            </section>
+
+            <?php if( !isset($_SESSION['login']) ) { ?>
+
+            <section class="navbtn">
+                <section class="submenu">
+                    <article class="topsubmenu">
+                        <a class="link1" href="connexion.php">Articles</a>
+                    </article>
+                    <article class="dropdownsubmenu">
+                        <a href="connexion.php">Test</a>
+                    </article>
                 </section>
             </section>
-            <section class="headerright">
-                  <?php if( !isset($_SESSION['login']) ){ ?>
-            <section class="undernav">
-                <a href="connexion.php">Connexion</a>
+            <section class="navbtn">
+                <a class="link1" href="connexion.php">Connexion</a>
             </section>
-            <section class="undernav">
-                <a href="inscription.php">Inscription</a>
+            <section class="navbtn">
+                <a class="link1" href="inscription.php">Inscription</a>
             </section>
-            <?php } if( isset($_SESSION['login']) ){ ?>
-             <section class="undernav">
-                <a href="profil.php">Profil</a>
+
+            <?php
+            } 
+            
+            if( isset($_SESSION['login']) ){ ?>
+             <section class="navbtn">
+                <a class="link1" href="profil.php">Profil</a>
             </section>
              <?php if( $_SESSION['droits'] == 42 || $_SESSION['droits'] == 1337 ){ ?>
-             <section class="undernav">
-                <a href="creer-article.php">Créer un Article</a>
+             <section class="navbtn">
+                <a class="link1" href="creer-article.php">Créer un Article</a>
             </section>
              <?php } ?>
               <?php if( $_SESSION['droits'] == 1337 ){ ?>
-            <section class="undernav">
-                <a href="admin.php">Admin</a>
+            <section class="navbtn">
+                <a class="link1" href="admin.php">Admin</a>
             </section>
              <?php } ?>
-            <section class="undernav">
-                <a href="index.php?deco">Déconnexion</a>
+            <section class="navbtn">
+                <a class="link1" href="index.php?deco">Déconnexion</a>
             </section>
             <?php } ?>
-            </section>
         </nav>
-    </header>
+</header>
